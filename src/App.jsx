3d968f7733b56ef685361ba85f5459e00a6e7076
sagar sangwan <iamsagarsangwan@gmail.com>
2025-08-28 18:35:36 +0530

    added a option if sentence is good with no errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import TextInput from "./components/text-input/text-input";
 import Button from "./components/button/button";
 import ApiError from "./components/error/api-error";
 import ApiErrorBox from "./components/error/api-error";
+import SuggestionCard from "./components/suggestion/suggestion-card";
+import NoErrorInText from "./components/suggestion/no-error-section";
 
 function App() {
   const [query, setQuery] = useState("hi, i am sagar");
   const [loading, setLoading] = useState(false);
-  const [suggestions, setSuggestions] = useState("");
+  const [matches, setMatches] = useState([]);
+  const [allGood, setAllGood] = useState("");
   const [apiError, setApiError] = useState("");
   console.log(apiError);
   const onSubmit = async () => {
@@ -23,7 +26,16 @@ function App() {
         return;
       }
       const data = await responnse.json();
-      setSuggestions(data.matches);
+      if (data.matches.length > 0) {
+        setApiError("");
+        setMatches(data.matches);
+        setAllGood("no");
+      } else {
+        console.log(data.matches);
+        setAllGood("yes");
+        setApiError("");
+        setMatches([]);
+      }
     } catch (e) {
       console.log(e);
       setApiError("Somethong went wrong try after sometime");
@@ -37,6 +49,7 @@ function App() {
       <div></div>
       {apiError && <ApiErrorBox message={apiError} />}
       <TextInput query={query} setQuery={setQuery} />
+
       <Button
         onClick={onSubmit}
         type="submit"
@@ -44,6 +57,8 @@ function App() {
       >
         {!loading ? "Submit" : "Loading Suggestions"}
       </Button>
+      {allGood == "yes" && <NoErrorInText />}
+      {matches.length > 0 && <SuggestionCard query={query} matches={matches} />}
     </div>
   );
 }
---
+++ src/components/suggestion/no-error-section.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./suggestion.css";
+function NoErrorInText() {
+  return (
+    <div>
+      <p className="text-success">Your sentence looks good!</p>
+    </div>
+  );
+}
+
+export default NoErrorInText;
