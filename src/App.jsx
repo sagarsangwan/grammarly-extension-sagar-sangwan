8cc02f18b855578cc215a7ce11cf5c0dba5b6eb8
sagar sangwan <iamsagarsangwan@gmail.com>
2025-08-28 14:41:56 +0530

    added a loading state

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import TextInput from "./components/text-input/text-input";
 import Button from "./components/button/button";
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("hi, i am sagar");
+  const [loading, setLoading] = useState(false);
+  const onSubmit = () => {
+    console.log("clickedd");
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    console.log("after clickedd");
+  };
 
   return (
     <div>
       <TextInput query={query} setQuery={setQuery} />
-      <Button
-        onClick={() => console.log(query)}
-        type="submit"
-        disabled={query.length <= 10}
-      >
-        Submit
+      <Button onClick={onSubmit} type="submit" disabled={query.length <= 10}>
+        {!loading ? "Submit" : "Loading Suggestions"}
       </Button>
     </div>
   );
